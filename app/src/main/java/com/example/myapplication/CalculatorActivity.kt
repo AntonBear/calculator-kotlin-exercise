8e4a2970f96f3c9b12093297e549948266a20171
AntonBear <holzed15@gmail.com>
2024-09-26 23:35:47 +0300

    fixed review issues

--- app/src/main/java/com/example/myapplication/CalculatorActivity.kt
+++ app/src/main/java/com/example/myapplication/CalculatorActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -21,16 +22,12 @@ class CalculatorActivity : AppCompatActivity() {
             binding.displayTextView.text = newDisplayText
         }
 
-        binding.buttonNumber0.setOnClickListener { viewModel.appendDigit("0") }
-        binding.buttonNumber1.setOnClickListener { viewModel.appendDigit("1") }
-        binding.buttonNumber2.setOnClickListener { viewModel.appendDigit("2") }
-        binding.buttonNumber3.setOnClickListener { viewModel.appendDigit("3") }
-        binding.buttonNumber4.setOnClickListener { viewModel.appendDigit("4") }
-        binding.buttonNumber5.setOnClickListener { viewModel.appendDigit("5") }
-        binding.buttonNumber6.setOnClickListener { viewModel.appendDigit("6") }
-        binding.buttonNumber7.setOnClickListener { viewModel.appendDigit("7") }
-        binding.buttonNumber8.setOnClickListener { viewModel.appendDigit("8") }
-        binding.buttonNumber9.setOnClickListener { viewModel.appendDigit("9") }
+        binding.numbersGroup.referencedIds.forEach {
+            findViewById<TextView>(it)?.let { textView ->
+                textView.setOnClickListener { viewModel.appendDigit(textView.text.toString()) }
+            }
+        }
+
         binding.buttonAc.setOnClickListener { viewModel.clear() }
         binding.buttonDot.setOnClickListener { viewModel.appendDigit(".") }
         binding.buttonMultiply.setOnClickListener {
